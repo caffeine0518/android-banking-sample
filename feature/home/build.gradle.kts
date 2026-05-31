e401e2d6d5f18df7375397f51bb63819a1141240
caffeine0518 <295715583+caffeine0518@users.noreply.github.com>
2026-05-31 12:36:06 +0900

    refactor: core-ui:common 모듈을 core-ui:mvi로 전환
    - common 모듈의 god-module 가능성 차단
    - pure kotlin class의 불필요한 의존성 제거

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -27,7 +27,15 @@ android {
 
 dependencies {
     implementation(projects.domain)
-    implementation(projects.coreUi.common)
+    implementation(projects.coreUi.mvi)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
